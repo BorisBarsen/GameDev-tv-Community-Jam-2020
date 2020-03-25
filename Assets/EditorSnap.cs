57ad791e5cc94b5976d31d2bf8f7845df707dbfc
BorisBarsen <boris1asenov@gmail.com>
2020-03-25 20:09:07 +0100

    Initial commit

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Awake");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update");
+    }
+}
