6389608d3da9ee9ae108cfa36d4eb03bf76e6a2f
Boris Asenov <boris1asenov@gmail.com>
2020-05-14 20:02:53 +0200

    Add a path of waypoints which we patrol and log to the console

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -3,18 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
 {
-    [Range(1f, 20f)]
-    [SerializeField] float gridSize = 10f;
-    // Update is called once per frame
+    [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
+
+    TextMesh textMesh;
+
     void Update()
     {
         Vector3 snapPos;
-        print(transform.position.x);
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
 
+        textMesh = GetComponentInChildren<TextMesh>();
+        string labelText = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+        textMesh.text = labelText;
+        gameObject.name = "Cube [" + labelText + "]";
+
+
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
     }
 }
