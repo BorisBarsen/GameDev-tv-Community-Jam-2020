1e26fc1653a1d52ab86b976100577b14ccd55479
Boris Asenov <boris1asenov@gmail.com>
2020-05-16 13:15:41 +0200

    Add in pathfinder and color in start and end waypoint

---
+++ Assets/PathFinder.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinder : MonoBehaviour
+{
+    [SerializeField] Waypoint startWaypoint;
+    [SerializeField] Waypoint endWaypoint;
+    Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+
+    // Use this for initialization
+    void Start()
+    {
+        LoadBlocks();
+    }
+
+    private void LoadBlocks()
+    {
+        Waypoint[] waypoints = FindObjectsOfType<Waypoint>();
+
+        foreach (Waypoint waypoint in waypoints)
+        {
+            if (!grid.ContainsKey(waypoint.GetGridPos()))
+            {
+                grid.Add(waypoint.GetGridPos(), waypoint);
+                SetTopColor(waypoint);
+            }
+            else
+            {
+                Debug.LogWarning("Skipping overlapping block " + waypoint);
+            }
+        }
+
+        print("Loaded " + grid.Count + " blocks");        
+    }
+
+    private void SetTopColor(Waypoint waypoint)
+    {
+        if (waypoint.Equals(startWaypoint))
+        {
+            waypoint.SetTopColor(Color.green);
+        }
+        else if (waypoint.Equals(endWaypoint))
+        {
+            waypoint.SetTopColor(Color.red);
+        }
+        else
+        {
+            waypoint.SetTopColor(Color.gray);
+        }
+    }
+}
--- Assets/Waypoint.cs
+++ Assets/Waypoint.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    Vector2Int gridPos;
+
+    const int gridSize = 10;
+
+    public int GetGridSize()
+    {
+        return gridSize;
+    }
+
+    public Vector2Int GetGridPos()
+    {
+        return new Vector2Int(
+            Mathf.RoundToInt(transform.position.x / gridSize) * gridSize,
+            Mathf.RoundToInt(transform.position.z / gridSize) * gridSize
+            );
+    }
+
+    public void SetTopColor(Color color)
+    {
+        MeshRenderer topMeshRenderer = transform.Find("Top").GetComponent<MeshRenderer>();
+        topMeshRenderer.material.color = color;
+    }
 }
