7d5d72dad99f89edc42fb3913bc8561ff33c8da5
Boris Asenov <boris1asenov@gmail.com>
2020-05-18 17:05:00 +0200

    [143] Before big solo challenge

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour {
+
+    [SerializeField] Transform objectToPan;
+    [SerializeField] Transform targetEnemy;
+
+    ParticleSystem gun;
+
+	// Use this for initialization
+	void Start () {
+        objectToPan = transform.Find("Tower_A_Top");
+        targetEnemy = GameObject.Find("Enemy").transform;
+
+        gun = transform.GetComponent<ParticleSystem>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        objectToPan.LookAt(targetEnemy);
+        if (Input.GetKeyDown("space"))
+        {
+            print("hello");
+            gun.Play();
+        }
+	}
+}
