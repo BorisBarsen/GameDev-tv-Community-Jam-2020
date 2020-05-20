936212932348d1b2dc276048f79d752ae7d45282
Boris Asenov <boris1asenov@gmail.com>
2020-05-20 22:33:29 +0200

    [148] Try coroutine for enemy spawning

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    [SerializeField] float secondsBetweenSpawns = 2f;
+    [SerializeField] EnemyMovement enemyPrefab;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(SpawnEnemy());
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(secondsBetweenSpawns);
+            GameObject enemyClone = Instantiate(enemyPrefab.gameObject, transform.position, transform.rotation) as GameObject;
+            print("Spawning!");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+}
--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -16,8 +16,7 @@ public class Tower : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        //objectToPan = transform.Find("Tower_A_Top");
-        //gun = gun;
+
     }
 	
 	// Update is called once per frame
