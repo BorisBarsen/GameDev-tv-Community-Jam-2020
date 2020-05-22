e3824539afa5358b595f0fe3a9db2aa49c50ec90
Boris Asenov <boris1asenov@gmail.com>
2020-05-22 16:57:47 +0200

    Move enemies without snapping

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,21 +5,39 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    [SerializeField] float travelTimePerBlock = 3f;
+    float timeTraveledToDestination;
+
+    Vector3 from;
+    Vector3 to;
+    
+    // Use this for initialization
+    void Start () {
         PathFinder pathfinder = FindObjectOfType<PathFinder>();
         var path = pathfinder.GetPath();
-        StartCoroutine(FollowPath(path));
+        StartCoroutine(UpdateDestination(path));
     }
 
-    IEnumerator FollowPath(List<Waypoint> path)
+    IEnumerator UpdateDestination(List<Waypoint> path)
     {
         print("Start patrol");
-        foreach (Waypoint waypoint in path)
+
+        for (int i=0; i < path.Count - 1; i++)
         {
-            gameObject.transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(1f);
+            from = path[i].transform.position;
+            to = path[i+1].transform.position;
+
+            timeTraveledToDestination = 0f;
+            yield return new WaitForSeconds(travelTimePerBlock);
         }
         print("End patrol");
+    }
+
+    private void Update()
+    {
+        float distanceThisFrame = (timeTraveledToDestination / travelTimePerBlock);
+        transform.position = from + ((from - to) * -1 * distanceThisFrame) ; // TODO fix moving off screen
+
+        timeTraveledToDestination += Time.deltaTime;
     }
 }
