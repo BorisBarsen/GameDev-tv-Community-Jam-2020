70ef73229fb24d4ccfdf0833e6b1174f58da01e6
Boris Asenov <boris1asenov@gmail.com>
2020-05-28 18:18:06 +0200

    Release version 1

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -46,8 +46,6 @@ public class Waypoint : MonoBehaviour {
         {
             if (isPlaceable)
             {
-                print("here");
-
                 FindObjectOfType<TowerFactory>().AddFireTower(this);
             }
             else
