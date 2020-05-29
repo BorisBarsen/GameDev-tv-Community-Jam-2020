af69f8ee26fb03aed56cda5d17aa6513e5ece0a5
Boris Asenov <boris1asenov@gmail.com>
2020-05-29 19:52:23 +0200

    Github desktop

---
+++ Assets/Scripts/EnemyTemperature.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTemperature : MonoBehaviour {
+
+    [Range (-3, 3)]
+    [SerializeField] float temperature = 0;
+    [SerializeField] ParticleSystem particleSystem;
+    [SerializeField] SeasonalCycle seasonalCycle;
+
+    [SerializeField] float hitChangeFactor;
+
+    [Range(0, 1)]
+    [SerializeField] float tempChangeFactor = 1f;
+    [SerializeField] Vector2 temperatureRange = new Vector2(-3f, 3f);
+    [SerializeField] Vector2 particleStartSpeedRange = new Vector2(3f, 15f);
+    [SerializeField] Vector2 particleSimulationSpeedRange = new Vector2(0.1f, 1.3f);
+
+    float seasonalTemp = 0f; 
+
+    ParticleSystem.MainModule particleSystemMain;
+    float oldTemp = 0;
+
+    float particleStartSpeed = 9f;
+    float particleSimulationSpeed = 0.7f;
+
+    float normalizedTemp;
+    float tempLerpValue;
+
+    public void SetSeasonalTemp(float amount)
+    {
+        seasonalTemp = amount;
+    }
+
+    // Use this for initialization
+    void Start () {
+        particleSystemMain = particleSystem.main;
+        particleSystemMain.startSpeed = particleStartSpeed;
+        particleSystemMain.simulationSpeed = particleSimulationSpeed;
+        seasonalTemp = seasonalCycle.temperature;
+        StartCoroutine(SeasonalTempChange());
+    }
+
+    IEnumerator SeasonalTempChange()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+            ChangeTemp(seasonalTemp);
+        }
+    }
+
+    public void ChangeTemp(float amount)
+    {
+        temperature += amount * tempChangeFactor;
+
+        if (temperature < temperatureRange.x) temperature = temperatureRange.x;
+        //if (temperature > temperatureRange.y) temperature = temperatureRange.y;//so it can explode after max reached
+
+        if (temperature >= temperatureRange.y)
+        {
+            GetComponent<EnemyHealth>().KillEnemy();
+        }
+        else
+        {
+            normalizedTemp = (temperature - temperatureRange.x) / (temperatureRange.y - temperatureRange.x);
+            GetComponent<EnemyMovement>().SetSpeedFactor(normalizedTemp);
+
+            LerpParticleSettings();
+
+            particleSystemMain.startSpeed = particleStartSpeed;
+            particleSystemMain.simulationSpeed = particleSimulationSpeed;
+        }
+
+    }
+
+    private void LerpParticleSettings()
+    {
+        particleStartSpeed = Mathf.Lerp(particleStartSpeedRange.x, particleStartSpeedRange.y, normalizedTemp);
+        particleSimulationSpeed = Mathf.Lerp(particleSimulationSpeedRange.x, particleSimulationSpeedRange.y, normalizedTemp);
+    }
+}
---
+++ Assets/Scripts/Season.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Season : MonoBehaviour {
+
+    public Season next;
+
+    [Tooltip("Temperature change per second")]
+    public float temperature = 0f;
+
+    [SerializeField] List<Texture> textures;
+    [SerializeField] ParticleSystem friendlyTowerParticles;
+
+    public ParticleSystem GetFriendlyBaseParticles()
+    {
+        return friendlyTowerParticles;
+    }
+
+    public List<Texture> GetTextures()
+    {
+        return textures;
+    }
+}
